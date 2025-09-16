5c79ff6f297aad914a640cc3fa2f5fb9709f0e84
Jean Cloude Claus <engr.jclaus@gmail.com>
2025-09-17 00:30:53 +0800

    added a parent and child route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import Homecards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import { 
+  Route, 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider, 
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+  <Route index element={<HomePage />} />
+  </Route>
+ )
+);
+
+
 const App = () => {
-  return (
-   <>
-   <NavBar />
-   <Hero />
-   <Homecards />
-   <JobListings />
-   <ViewAllJobs />
-
-    
-
-   </>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App
