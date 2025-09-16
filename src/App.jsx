a59668e2b84c09c3c189f0c99a7f6ff37f17d6e6
Jean Cloude Claus <engr.jclaus@gmail.com>
2025-09-16 22:03:19 +0800

    Added a joblist requirement

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Homecards from './components/HomeCards'
-import JobListing from './components/JobListing'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 const App = () => {
   return (
    <>
    <NavBar />
    <Hero />
    <Homecards />
-   <JobListing />
+   <JobListings />
+   <ViewAllJobs />
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    
 
    </>
   )
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import jobs from '../jobs.json'
+import {useState} from 'react'
+import { FaMapMarker } from 'react-icons/fa'
 
-function JobListing() {
+
+const JobListing = ({ job }) => {
+    const [showFullDescription, setShowFullDescription] = useState
+    (false);
+
+    let description = job.description;
+
+    if(!showFullDescription) {
+        description = description.substring(0, 90) + '...';
+    }
+
   return (
-   <section className="bg-blue-50 px-4 py-10">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            { jobs.map((job) => (
-                <div className="bg-white rounded-xl shadow-md relative">
+    <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
               <div className="mb-6">
-                <div className="text-gray-600 my-2">Full-Time</div>
-                <h3 className="text-xl font-bold">Senior React Developer</h3>
+                <div className="text-gray-600 my-2">{job.type}</div>
+                <h3 className="text-xl font-bold">{job.title}</h3>
               </div>
 
               <div className="mb-5">
-               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+               { description }
               </div>
 
-              <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+              <button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">
+                { showFullDescription ? 'Less' : 'More'}
+              </button>
+
+              <h3 className="text-indigo-500 mb-2">{job.salary}/ Year</h3>
 
               <div className="border border-gray-100 mb-5"></div>
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
-                  <i className="fa-solid fa-location-dot text-lg"></i>
-                  Boston, MA
+                  <FaMapMarker className='inline text-lg mb-1 mr-1' />
+                  { job.location }
                 </div>
                 <a
-                  href="job.html"
+                  href={ `/job/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
@@ -39,10 +46,6 @@ function JobListing() {
               </div>
             </div>
           </div>
-        )) }
-        </div>
-      </div>
-    </section>
   )
 }
 
